948156fc93dd475f41dd2f018b49cee34e668626
alex belle <belletskiy75@mail.com>
2024-02-23 22:20:36 +0300

    add Lesson1:Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 33;
+int b = 71;
+int c = 22;
+int d = 99;
+int e = 16;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
+
