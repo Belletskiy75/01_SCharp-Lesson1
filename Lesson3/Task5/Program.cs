80361ede464cfa63f573e5fe54779fead267d9da
alex belle <belletskiy75@mail.com>
2024-06-16 21:18:25 +0300

    Lesson3:Task5

---
+++ Lesson3/Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿int size = 10;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine(" Ведите число с консоли: ");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i++;
+}
