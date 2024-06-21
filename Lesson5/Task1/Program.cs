36997a281b0a858abdf3ee95d5aa0429208d31b0
alex belle <belletskiy75@mail.com>
2024-06-21 20:55:15 +0300

    Lesson5:Task1

--- Lesson5/Task1/Program.cs
+++ Lesson5/Task1/Program.cs
@@ -1,10 +1,14 @@
-﻿int Fact(int n)
+﻿
+
+int Fact(int n)
 {
     if (n == 1 || n == 0)
     {
+        Console.WriteLine($"stop {n}");
         return 1;
     }
+    Console.WriteLine(n);
     return n * Fact(n - 1);
 }
 
-Console.Write(Fact(6));
+Console.Write(Fact(5));
